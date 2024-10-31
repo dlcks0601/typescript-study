4215eac94f68c8887772a3a26b1c5efbac88bc98
leechan <dlcks0601@gmail.com>
2024-10-31 22:03:12 +0900

    typescript-study : 투두리스트

--- react/todolist/src/App.tsx
+++ react/todolist/src/App.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ClassCom from './ClasCom';
+import TodoList from './Todolist';
 
 function App() {
-  let name = '리액트';
-
-  // in-line 스타일
-
-  // const style = {
-  //   background: 'black',
-  //   color: 'white',
-  //   fontSize: '48px',
-  //   fontWeight: 'bold',
-  //   padding: '20px',
-  // };
-
   return (
-    <div className='container'>
-      <h1 className='test'>
-        Hello,
-        {name === '리액트' ? <h1>YES</h1> : null}!!
-      </h1>
-      <p>반갑습니다.</p>
+    <div className='App'>
+      <h1 style={{ backgroundColor: 'white' }}>Hello React</h1>
+      <TodoList />
     </div>
   );
 }
